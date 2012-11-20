313dbcc1a27efbc8dbf3f32ebb925f9f327991d5
Dody Gunawinata <dodyg@silverkeytech.com>
2012-11-20 16:43:25 +0200

    Add HttpRequest and add Internet permission for the application.

--- src/com/silverkeytech/android_rivers/MainActivity.kt
+++ src/com/silverkeytech/android_rivers/MainActivity.kt
@@ -2,10 +2,28 @@ package com.silverkeytech.android_rivers
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
+import roboguice.inject.InjectView
+import roboguice.activity.RoboActivity
+import com.github.kevinsawicki.http
+import com.github.kevinsawicki.http.HttpRequest
+import android.util.Log
 
 public open class MainActivity(): Activity() {
+    class object {
+        public val TAG: String = javaClass<MainActivity>().getSimpleName()!!
+    }
+
     public override fun onCreate(savedInstanceState: Bundle?): Unit {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.main)
+        load()
+    }
+
+    fun load(){
+        Log.d(TAG, "Starting to load")
+        var req = HttpRequest.get("http://hobieu.apphb.com/api/1/default/riverssubscription")?.body()
+        Log.d(TAG, "Loading content ${req?.length()}")
     }
 }
