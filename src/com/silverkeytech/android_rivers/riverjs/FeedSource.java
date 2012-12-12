0d07f31df4f8730956b97dc74a86a0a4478fd430
Dody Gunawinata <dodyg@silverkeytech.com>
2012-12-12 11:18:50 +0200

    Update license information
    Add GPL 3 notice in all relevant code.

--- src/com/silverkeytech/android_rivers/riverjs/FeedSource.java
+++ src/com/silverkeytech/android_rivers/riverjs/FeedSource.java
@@ -1,12 +1,23 @@
+/*
+Android Rivers is an app to read and discover news using RiverJs, RSS and OPML format.
+Copyright (C) 2012 Dody Gunawinata (dodyg@silverkeytech.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
 package com.silverkeytech.android_rivers.riverjs;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Dody
- * Date: 10/12/12
- * Time: 16:51
- * To change this template use File | Settings | File Templates.
- */
 public class FeedSource {
     public FeedOpml opml;
 }
