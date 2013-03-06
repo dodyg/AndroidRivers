2eae1920b680cdd6aa0afce34a1aa4530130534e
Dody Gunawinata <dodyg@silverkeytech.com>
2013-03-06 03:30:15 +0200

    Handle audio focus.

--- src/com/silverkeytech/android_rivers/PodcastPlayerService.kt
+++ src/com/silverkeytech/android_rivers/PodcastPlayerService.kt
@@ -19,11 +19,17 @@ import android.support.v4.app.NotificationCompat
 import java.util.Random
 import android.app.NotificationManager
 import android.content.Context
+import android.media.AudioManager
+import android.media.AudioManager.OnAudioFocusChangeListener
+import android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT
+import android.media.AudioManager.AUDIOFOCUS_GAIN
+import android.media.AudioManager.AUDIOFOCUS_LOSS
 
-public open class PodcastPlayerService(): Service(), MediaPlayer.OnErrorListener{
+public open class PodcastPlayerService(): Service(), MediaPlayer.OnErrorListener, OnAudioFocusChangeListener {
     class object{
         public val TAG: String = javaClass<PodcastPlayerService>().getSimpleName()
     }
+
 
     private val binder: IBinder = ServiceBinder()
     private var mPlayer: MediaPlayer? = null
@@ -55,7 +61,7 @@ public open class PodcastPlayerService(): Service(), MediaPlayer.OnErrorListener
 
         notification.contentView = RemoteViews(getApplicationContext()!!.getPackageName(), R.layout.download_progress).with {
             this.setImageViewResource(R.id.download_progress_status_icon, android.R.drawable.btn_star)
-            this.setProgressBar(R.id.download_progress_status_progress, 100, 0, false)
+            this.setProgressBar(R.id.download_progress_status_progress, 100, 100, true)
             this.setTextViewText(R.id.download_progress_status_text, getString(R.string.download_starts))
         }
 
@@ -68,39 +74,55 @@ public open class PodcastPlayerService(): Service(), MediaPlayer.OnErrorListener
         notification.contentView!!.setTextViewText(R.id.download_progress_status_text, msg)
     }
 
-    fun updateProgress(notification : Notification, sofar: Int) {
-        notification.contentView!!.setProgressBar(R.id.download_progress_status_progress, 100, sofar, true)
-    }
+    var audioManager : AudioManager? = null
 
+    //http://developer.android.com/training/managing-audio/audio-focus.html
     public override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val podcastTitle = intent!!.getStringExtra(Params.PODCAST_TITLE)
         val podcastPath = intent.getStringExtra(Params.PODCAST_PATH)
 
         val notification = prepareNotification()
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        updateText(notification, "Playing $podcastTitle")
-        notificationManager.notify(notificationId, notification)
 
-        Log.d(TAG, "Path $podcastPath")
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        val result = audioManager?.requestAudioFocus(this, AudioManager.STREAM_MUSIC,AudioManager.AUDIOFOCUS_GAIN);
 
-        mPlayer = MediaPlayer.create(this, Uri.parse(podcastPath))
-        mPlayer?.setOnErrorListener(this)
-        mPlayer?.setLooping(false)
-        mPlayer?.setVolume(100.0, 100.0)
-        mPlayer?.start()
+        if (result != AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+            updateText(notification, "Sorry, I cannot play $podcastTitle at this moment")
+            notificationManager.notify(notificationId, notification)
+        }
+        else{
+            updateText(notification, "Playing $podcastTitle")
+            notificationManager.notify(notificationId, notification)
 
-        mPlayer!!.setOnCompletionListener(object: MediaPlayer.OnCompletionListener{
-            public override fun onCompletion(p0: MediaPlayer?) {
-                updateText(notification, "Podcast completed")
-                updateProgress(notification, 100)
-                notificationManager.notify(notificationId, notification)
-            }
-        })
+            mPlayer = MediaPlayer.create(this, Uri.parse(podcastPath))
+            mPlayer?.setOnErrorListener(this)
+            mPlayer?.setLooping(false)
+            mPlayer?.setVolume(100.0, 100.0)
+            mPlayer?.start()
+
+            mPlayer!!.setOnCompletionListener(object: MediaPlayer.OnCompletionListener{
+                public override fun onCompletion(p0: MediaPlayer?) {
+                    updateText(notification, "Podcast completed")
+                    notificationManager.notify(notificationId, notification)
+                }
+            })
+        }
 
         return super<Service>.onStartCommand(intent, flags, startId)
     }
 
-
+    public override fun onAudioFocusChange(p0: Int) {
+        if (p0 == AUDIOFOCUS_LOSS_TRANSIENT){
+            this.pauseMusic()
+        }
+        else if (p0 == AudioManager.AUDIOFOCUS_GAIN) {
+            this.resumeMusic()
+        } else if (p0 == AudioManager.AUDIOFOCUS_LOSS) {
+            audioManager?.abandonAudioFocus(this)
+            this.stopMusic()
+        }
+    }
 
     public fun isPlaying() : Boolean{
         if (mPlayer != null)
