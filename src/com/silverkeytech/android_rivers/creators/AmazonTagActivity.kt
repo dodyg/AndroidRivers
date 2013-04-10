51cbc8993b0e7a93e33ad228b559d9caf6c86ae2
Dody Gunawinata <dodyg@silverkeytech.com>
2013-04-10 14:01:43 +0200

    Add skeleton for amazon tag activity

---
+++ src/com/silverkeytech/android_rivers/creators/AmazonTagActivity.kt
@@ -0,0 +1,14 @@
+package com.silverkeytech.android_rivers.creators
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Dody
+ * Date: 10/04/13
+ * Time: 13:56
+ * To change this template use File | Settings | File Templates.
+ */
+
+//http://www.amazon.com/gp/tagging/rss-help.html
+//http://www.amazon.com/rss/tag/apple%20computer/new/
+//http://www.amazon.com/rss/tag/sci-fi/popular/ref=tag_rsh_hl_erso
+//http://www.amazon.com/rss/tag/running/recent/ref=tag_rsh_hl_ersr
